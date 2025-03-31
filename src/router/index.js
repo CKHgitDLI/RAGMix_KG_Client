91c610cefbe6bce0d56a8d74c74d6ceca8195f43
ckh <ckh@ckh-cn.site>
2025-04-01 02:23:07 +0800

    基本实现前端页面的展示

---
+++ src/router/index.js
@@ -0,0 +1,29 @@
+/**
+ * Created by chendaye666 on 17/8/5.
+ */
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
+
+export default new VueRouter({
+	mode: 'history',
+	linkActiveClass: 'is-active',
+	scrollBehavior: () => ({
+		y: 0
+	}),
+	routes: [{
+		path: '/',
+		component: require('../views/Main')
+	}
+	// , {
+	// 	name: 'main',
+	// 	path: '/main',
+	// 	component: require('../views/Main')
+	// },
+	// {
+	// 	name: 'neo4j',
+	// 	path: '/neo4j',
+	// 	component: require('../views/neo4j')
+	// }
+]
+})
