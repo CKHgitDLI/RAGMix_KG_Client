80b2b66e0b303fe4540a3361c7e3846c380ca2fe
ckh <ckh@ckh-cn.site>
2025-04-15 11:59:34 +0800

    backup-20250415--1158

---
+++ vite.config.js
@@ -0,0 +1,24 @@
+import {fileURLToPath, URL} from 'node:url'
+
+import {defineConfig} from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+import vueJsxPlugin from "@vitejs/plugin-vue-jsx";
+
+// https://vite.dev/config/
+export default defineConfig({
+    plugins: [
+        vue(),
+        vueDevTools(),
+        vueJsxPlugin()
+    ],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        },
+        extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue']
+    },
+    esbuild: {
+        loader: "jsx",
+    },
+})
